baa0a5f45ced463e14e467bc81ad7996ccc3429f
FreeGIS <free_gis@qq.com>
2021-12-24 07:50:22 +0800

    readme

--- gen/exampleLoader.js
+++ gen/exampleLoader.js
@@ -6,14 +6,14 @@
   export const indices = async () => (await import('../src/examples/indices'));
   
 
-  export const dem = async () => (await import('../src/examples/dem'));
-  
-
-  export const dem_light = async () => (await import('../src/examples/dem_light'));
-  
-
   export const uv_texture = async () => (await import('../src/examples/uv_texture'));
   
 
   export const geo_texture = async () => (await import('../src/examples/geo_texture'));
+  
+
+  export const dem = async () => (await import('../src/examples/dem'));
+  
+
+  export const dem_light = async () => (await import('../src/examples/dem_light'));
   
